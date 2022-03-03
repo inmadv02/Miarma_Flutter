2b7d77d579c5b7358bca9818cdced420aac353d6
María Inmaculada Domínguez Vargas <onemayn@outlook.es>
2022-03-03 21:54:27 +0100

    grid view

--- miarma_app_flutter/lib/src/ui/pages/profile_page.dart
+++ miarma_app_flutter/lib/src/ui/pages/profile_page.dart
@@ -180,35 +180,46 @@ class _ProfilePageState extends State<ProfilePage> {
                 ],
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  SizedBox(
-                      width: 90,
-                      height: 150,
-                      child: Image(
-                        image: AssetImage("assets/images/post1.jpg"),
-                        fit: BoxFit.contain,
-                      )),
-                  SizedBox(
-                    width: 20,
+                children: [
+                  GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 1,
+                    mainAxisSpacing: 1,
+                    crossAxisCount: 3,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text("He'd have you all unravel at the"),
+                        color: Colors.teal[100],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Heed not the rabble'),
+                        color: Colors.teal[200],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Sound of screams but the'),
+                        color: Colors.teal[300],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Who scream'),
+                        color: Colors.teal[400],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Revolution is coming...'),
+                        color: Colors.teal[500],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Text('Revolution, they...'),
+                        color: Colors.teal[600],
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                      width: 100,
-                      height: 150,
-                      child: Image(
-                        image: AssetImage("assets/images/post1.jpg"),
-                        fit: BoxFit.contain,
-                      )),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  SizedBox(
-                      width: 120,
-                      height: 150,
-                      child: Image(
-                        image: AssetImage("assets/images/post1.jpg"),
-                        fit: BoxFit.contain,
-                      ))
                 ],
               )
             ],
